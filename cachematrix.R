cb4ac9be4780bf58049892b25389ea0aae9ff310
yifeili <syfifi1988@hotmail.com>
2016-04-18 18:02:35 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,12 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function (y){
+                x <<- y
+                m <<- NULL
+        }
+        get 
 }
 
 
